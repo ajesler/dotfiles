836921073d42351a7722372dc0ad2bccc594093f
ajesler <aj@esler.co.nz>
2017-04-24 20:00:06 +1200

    Add failure message to install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 set -x
 
+function failure() {
+  set +x
+  # TODO Make this red
+  echo "Installation failed!"
+  exit 1
+}
+
 if [[ ! -d $HOME/bin ]]; then
-  mkdir $HOME/bin
+  mkdir $HOME/bin || failure
 fi
 
-stow --target=$HOME zsh vim git tmux
+stow --target=$HOME zsh vim git tmux || failure
 
 # This is a bit special as we might have machine specific files here already
-stow --target=$HOME/bin -R bin
+stow --target=$HOME/bin -R bin || failure
