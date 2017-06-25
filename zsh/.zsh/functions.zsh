65a0f275903b9dc8d0d85e0d2a41ab68fb1b9978
ajesler <andrew.esler@powershop.co.nz>
2017-06-26 10:48:46 +1200

    Add echo_magenta, echo_red, echo_blue, and echo_green functions

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -9,3 +9,19 @@ fancy-ctrl-z () {
 }
 zle -N fancy-ctrl-z
 bindkey '^Z' fancy-ctrl-z
+
+echo_magenta () {
+  echo "$fg_no_bold[magenta]${@}$reset_color"
+}
+
+echo_red () {
+  echo "$fg_no_bold[red]${@}$reset_color"
+}
+
+echo_blue () {
+  echo "$fg_no_bold[blue]${@}$reset_color"
+}
+
+echo_green () {
+  echo "$fg_no_bold[green]${@}$reset_color"
+}
