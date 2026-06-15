41cb041561eaa3048263f0a9995710eedd78ccc9
AJ Esler <aj+github@hatchinvest.nz>
2026-06-16 10:46:18 +1200

    Update the prompt

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -7,3 +7,17 @@
 # }
 
 # PROMPT="%{$reset_color%}→ "
+
+# Load version control information
+autoload -Uz vcs_info
+precmd() { vcs_info }
+
+# Format the vcs_info_msg_0_ variable
+zstyle ':vcs_info:git:*' formats '%b'
+
+# Set up the prompt (with git branch name)
+setopt PROMPT_SUBST
+
+# export PS1="%1~ (${vcs_info_msg_0_}) $ "
+# PROMPT="%1~ (${vcs_info_msg_0_}) $ "
+PROMPT='[%1~](${vcs_info_msg_0_}) $ '
